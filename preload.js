5b842b68a1a626793da1a952ec271dad224ae7ac
Realferuzbek1 <feruzbekqurbonov03@gmail.com>
2025-10-17 16:27:52 +0500

    Initial commit (source only, no binaries)

---
+++ preload.js
@@ -0,0 +1,7 @@
+﻿import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  setCompactMode(compact) {
+    ipcRenderer.invoke('set-compact-mode', Boolean(compact));
+  }
+});
